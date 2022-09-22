3a217d8c1dce4a6c5af11f88be056ecc0c1e59d2
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-09-22 18:44:56 -0300

    Exercício 90

---
+++ Python/aula_19.py
@@ -0,0 +1,29 @@
+ #90
+# Dicionario
+'''
+aluno = dict()
+
+print('==='*10)
+print('Analisando situação do aluno')
+print('==='*10)
+
+aluno['nome'] = str(input('Nome: '))
+
+aluno['media'] =  float(input(f'Media de {aluno["nome"]}: '))
+
+if aluno['media'] >= 7:
+    aluno['situação'] =  'Aprovado'
+
+elif 5 <= aluno['media']< 7:
+    aluno['situação'] = 'Recuperação'
+
+else:
+    aluno['situação'] = 'Reprovado'
+
+print('==='*10)
+
+print(f'- Aluno {aluno["nome"]}')
+print(f'- Média do aluno é {aluno["media"]}')
+print(f'- Situação: {aluno["situação"]}')
+'''
+
