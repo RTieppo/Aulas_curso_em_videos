321a24e62f3c83ae0dcdb2945cbbbea16e75b666
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-09-28 20:49:45 -0300

    Exercicio 96

---
+++ Python/aula_20.py
@@ -0,0 +1,15 @@
+#Def
+'''
+#96
+#função que calcula área
+
+def calculo_area(l,c):
+    print(f'A área de um terreno {l}x{c} é de {l*c}m².')
+
+
+print ('Controle de Terreno')
+print('----'*10)
+
+calculo_area(float(input('Largura (m): ')), float(input('Comprimeto (m): ')))
+'''
+
