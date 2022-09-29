d2b4f4ab7f20556bbd6d60966eeab6982341bd3f
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-09-28 22:00:55 -0300

    Exercício 98

--- Python/aula_20.py
+++ Python/aula_20.py
@@ -24,4 +24,51 @@ def print_e(valor):
 
 print_e(str(input('Digite o alguma coisa: ')))
 '''
+'''
+#98
+#função de contagem
+from time import sleep
 
+
+def contador(inicial,fim,intervalo):
+
+    if intervalo == 0:
+        intervalo = 1
+    
+    if intervalo < 0:
+        intervalo *= -1
+
+    if inicial < 0:
+        inicial *= -1
+    
+    if fim < 0:
+        inicial *= -1
+
+    print('==='*10)
+    print(f'Contagem de {inicial} até {fim} de {intervalo} em {intervalo}')
+    
+    if inicial < fim:
+        for n in range(inicial,fim+1,intervalo):
+            print(f'{n}', end=' ', flush=True)
+            sleep(0.1)
+
+        print('FIM!')
+    
+    else:
+        conta = inicial
+        while conta >= fim:
+            print(f'{conta}', end=' ', flush=True)
+            conta -= intervalo
+            sleep(0.1)
+        print('FIM!')
+
+
+
+contador(1,10,1)
+contador(1,10,2)
+
+print('==='*10)
+print('Sua vez:')
+contador(int(input('Início: ')), int(input('Fim: ')), int(input('Passo: ')))
+
+'''
