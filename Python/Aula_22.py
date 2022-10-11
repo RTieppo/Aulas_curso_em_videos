27480baf3b06cd1839c0eddb2faf971f008f1b94
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-10-10 21:17:08 -0300

    exercício 107

---
+++ Python/Aula_22.py
@@ -0,0 +1,11 @@
+#Modularização em python
+''''
+#107
+
+from Modulos_aula_21 import exercicio_107
+
+valor = float(input('Digite o preço: R$'))
+print(f'A metade de R${valor} é R${exercicio_107.metade(valor)}')
+print(f'O dobro de R${valor} é R${exercicio_107.dobro(valor)}')
+print(f'Aumentando 10%, temos R${exercicio_107.porcento(valor)}')
+'''
