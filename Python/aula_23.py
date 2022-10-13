3c4d7cab43b6fbc235de07759192fd54bc316263
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-10-13 20:31:19 -0300

    Exercício 113

---
+++ Python/aula_23.py
@@ -0,0 +1,36 @@
+#113 
+#Melhorando função
+def leia_int(num):
+    while True:
+        try:
+            analise = int(input(num))
+
+        except ValueError:
+            print('ERRO! Valor não é valido.')
+        
+        except KeyboardInterrupt:
+            print('Usuario preferiu não continuar')
+            return 0
+
+        else:
+           return analise
+
+def leia_real(num):
+    while True:
+        try:
+            analise = int(input(num))
+        except ValueError:
+            print('ERRO! Valor não é valido.')
+        
+        except KeyboardInterrupt:
+            print('Usuario preferiu não continuar')
+            return 0
+        
+        else:
+            return analise
+
+valor_int = leia_int('Digite um valor inteiro: ')
+valor_real = leia_real('Digite um valor Real: ')
+print(f'O valor Inteiro foi {valor_int} e o real foi {valor_real}')
+
+ 
