a3a85b7884b90d3bd7fccfd0e12cab54590e3881
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-10-24 20:40:48 -0300

    aula 7 - aula 8

--- banco_de_dados/aula_banco_07.sql
+++ banco_de_dados/aula_banco_07.sql
@@ -1,15 +1,31 @@
-create table if not exists cursos (
-nome varchar(30) not null unique,
-descricao text,
-carga int unsigned,
-tataulas int unsigned,
-ano year default '2022'
-) default charset = utf8mb4;
+insert into cursos values
+('1','HTM4','Curso de HTML5','40','37','2014'),
+('2','Algoritmos','Lógica de Programação','20','15','2014'),
+('3','Photoshop','Dicas de Photoshop CC','10','8','2014'),
+('4','PGP','Curso de PHP para iniciantes','40','20','2010'),
+('5','Jarva','Introdução a Linguagem Java','10','29','2000'),
+('6','MySQL','Banco de dados MySQL','30','15','2016'),
+('7','Word','Curso completo de Word','40','30','2016'),
+('8','Sapateado','Danças Ritmicas','40','30','2018'),
+('9','Cozinha Àrabe','Aprenda a fazer kibe','40','30','2018'),
+('10','YouTuber','Gerar polêmica e ganhar inscritos','5','2','2018');
 
-alter table cursos
-add column idcurso int first;
+select * from cursos;
 
-alter table cursos
-add primary key(idcurso);
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
 
-drop table cursos;
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+update cursos
+set nome = 'java', ano ='2015', carga = '40'
+where idcurso = '5'
+limit 1;
+
+delete from cursos
+where idcurso = '10';
+
+truncate table cursos;
