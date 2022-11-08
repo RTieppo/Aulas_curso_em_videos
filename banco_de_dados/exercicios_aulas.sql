db8de52ad4fc06ff690d27d5f4aaebe7103aa6f8
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-11-08 18:12:09 -0300

    exercício aula 12
    finalizados

--- banco_de_dados/exercicios_aulas.sql
+++ banco_de_dados/exercicios_aulas.sql
@@ -11,5 +11,21 @@ select * from gafanhotos
 where profissao = 'Programador' and sexo = 'M'
 order by nome;
 
+select * from gafanhotos
+where nacionalidade = 'Brasil' and sexo = 'F' and nome like 'j%'
+order by nome;
 
+select nome, nacionalidade from gafanhotos
+where nome like '%silva' and nacionalidade <> 'Brasil' and peso < '100'
+order by nome;
 
+select max(altura) from gafanhotos where nacionalidade = 'Brasil' and sexo = 'M'; 
+
+select avg(peso) from gafanhotos;
+
+select nome, min(peso), nascimento, sexo from gafanhotos
+where nascimento between '1990-01-01' and '2000-12-31' and sexo = 'F'
+order by nome;
+
+select count(altura) from gafanhotos
+where altura > '1.9';
