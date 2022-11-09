735c0066bbb9a842875831caeecc79ca8e215089
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-11-08 21:51:45 -0300

    Create aula_13.sql

---
+++ banco_de_dados/aula_13.sql
@@ -0,0 +1,13 @@
+select totaulas, count(*) from cursos
+group by totaulas
+order by totaulas;
+
+select ano, count(*) from cursos
+group by ano
+having count(ano) >= 5
+order by count(*) desc;
+
+select carga, count(*) from cursos
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos);
