b4ef08ef60efaabded976693cb5bc3f6bc0d47c3
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2022-11-10 18:35:03 -0300

    Update aula_15.sql

--- banco_de_dados/aula_15.sql
+++ banco_de_dados/aula_15.sql
@@ -16,3 +16,18 @@ update gafanhotos set cursopreferido = '6' where id = '1';
 delete from cursos where idcurso = '9';
 
 drop database cadastro;
+
+select gafanhotos.nome, cursos.nome, cursos.ano
+from gafanhotos inner join cursos
+on cursos.idcurso = gafanhotos.cursopreferido;
+
+
+select gafanhotos.nome, cursos.nome, cursos.ano
+from gafanhotos left outer join cursos
+on cursos.idcurso = gafanhotos.cursopreferido
+order by gafanhotos.nome;
+
+select gafanhotos.nome, cursos.nome, cursos.ano
+from gafanhotos right outer join cursos
+on cursos.idcurso = gafanhotos.cursopreferido
+order by cursos.nome;
